78233b384a547f39e349772c1728bdf1082a54dc
satoru.akiyama <akich-tech@yahoo.co.jp>
2023-02-26 15:14:46 +0900

    課題：getAllMessagesを追加

--- src/models/Tasklist.java
+++ src/models/Tasklist.java
@@ -7,11 +7,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT t FROM Tasklist AS t ORDER BY t.id DESC"
+    )
+})
 @Table(name = "tasks")
-public class tasklist {
+public class Tasklist {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
